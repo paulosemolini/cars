66548a0fa7fdadf769ffeab0f3f41e63662d0449
Paulo Henrique <paulohenriquess@gmail.com>
2021-05-29 16:56:43 -0300

    Inicio do projeto

---
+++ lib/pages/login/login_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Carros'),
+      ),
+      body: _body(),
+    );
+  }
+
+  _body() {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
